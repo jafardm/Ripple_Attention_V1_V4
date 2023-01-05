0d716ccfb80c0f889dc78b22ca83e9d4e11a8b96
Jafar Doostmohammadi <52324230+jafardm@users.noreply.github.com>
2023-01-05 17:30:53 -0500

    Update spk_ripple.m

--- spk_ripple.m
+++ spk_ripple.m
@@ -1,13 +1,13 @@
 % spiking activity at the ripple time
 
-cd D:\SWR-Code
-addpath('D:\SWR-Code\NeuralynxFunctions\')
-addpath('D:\SWR-Code\NeuralynxFunctions\');
-addpath('D:\SWR-Code\NLXMatlabTools-master\');
-addpath('D:\SWR-Code\FMAToolbox\Helpers\');
-addpath('D:\SWR-Code\trialextraction-master\');
-addpath('D:\SWR-Code\cortex-matlabtoolbox-master\');
-addpath('D:\SWR-Code\SWR\')
+addpath('...\NeuralynxFunctions\');
+addpath('...\NeuralynxFunctions\');
+addpath('...\NLXMatlabTools-master\');
+addpath('...\FMAToolbox\Helpers\');
+addpath('...\trialextraction-master\');
+addpath('...\cortex-matlabtoolbox-master\');
+addpath('...\SWR\')
+
 
 %%
 clear
@@ -17,11 +17,11 @@ labelpath    = '...\Labels\';
 
 cfg.area  = 'e1';
 
-subjectName = 'Richard';
+subjectName = 'Monkey1';
 switch subjectName
-    case 'Caesar'
+    case 'Monkey1'
         pen  = CaesarDataInfo(datasetpath);
-    case   'Richard'
+    case   'Monkey2'
         pen  = RichardDataInfo(datasetpath);
 end
 
@@ -146,115 +146,7 @@ Pooled_Rate.WithoutAway = without_Away;
 Pooled_Rate.TrialIDs   = TrialsLabels;
 Pooled_Rate.session    = sessions;
 
-savename = fullfile('D:\SWR-Results\spikes-ripples-sorted',[subjectName(1) cfg.area]);
-save(savename,'Pooled_Rate')
 
-
-%% plot firing rate at ripple time
-clear
-load('D:\SWR-Results\spikes-ripples-sorted\Re1.mat')
-% average ripple in RF
-RippleRF = Pooled_Rate.rippleRF;
-% average ripple in without
-randomRF = Pooled_Rate.WithoutRF;
-% average ripple in away
-Rippleaway = Pooled_Rate.rippleAway; 
-% average ripple in away random
-randomaway = Pooled_Rate.WithoutAway;
-
-spkSF = 32552;
-t = (-0.1:1/spkSF:0.1)*1000;
-
-fig = figure; hold on
-mnRippleRF = nanmean(RippleRF,1);
-sd1 = std(RippleRF,0,1)/(sqrt(size(RippleRF,1))-1);
-boundedline(t,mnRippleRF,sd1,'cmap',[161 10 15]/255,'alpha')
-
-mnrandomRF = nanmean(randomRF,1);
-sd1 = std(randomRF,0,1)/(sqrt(size(randomRF,1))-1);
-boundedline(t,mnrandomRF,sd1,'cmap',[255 48 155]/255,'alpha')
-
-
-mnRipplAway = nanmean(Rippleaway,1);
-sd2 = std(Rippleaway,0,1)/(sqrt(size(Rippleaway,1))-1);
-boundedline(t,mnRipplAway,sd2,'cmap',[18 108 161]/255,'alpha')
-
-mnrandaway = nanmean(randomaway,1);
-sd2 = std(randomaway,0,1)/(sqrt(size(randomaway,1))-1);
-boundedline(t,mnrandaway,sd2,'cmap',[160 255 242]/255,'alpha')
-
-legend('Ripple RF','Ripple away','without ripple RF','without ripple away')
-box off 
-xlabel('Time(ms)')
-ylabel('Firing rate(Hz)')
- ylim([10 35])
-set(gca,'fontsize',8,'Fontname','Arial','TickDir', 'out'),box off
-fig.Renderer = 'painters';
-
-%%
-clear
-load('D:\SWR-Results\spikes-ripples-sorted\Ce2.mat')
-% average ripple in RF
-RippleM1   = mean(Pooled_Rate.rippleRF,2);
-RippleAwayM1 = mean(Pooled_Rate.rippleAway,2);
-
-WithotRFM1   = mean(Pooled_Rate.WithoutRF,2);
-WithotAwayM1 = mean(Pooled_Rate.WithoutAway,2);
-
-M1Rate      = [RippleM1;RippleAwayM1;WithotRFM1;WithotAwayM1];
-trialIdM1   = [Pooled_Rate.TrialIDs;Pooled_Rate.TrialIDs];
-sessionM1   = [Pooled_Rate.session; Pooled_Rate.session];
-
- rippleLabels1 = [ones(length(RippleM1),1);ones(length(RippleAwayM1),1);...
-     zeros(length(WithotRFM1),1);zeros(length(WithotAwayM1),1)];
-
-clear Pooled_Rate
-
-load('D:\SWR-Results\spikes-ripples-sorted\Re2.mat')
-
-% average ripple in RF
-RippleM2   = nanmean(Pooled_Rate.rippleRF,2);
-RippleAwayM2 = nanmean(Pooled_Rate.rippleAway,2);
-
-WithotRFM2   = nanmean(Pooled_Rate.WithoutRF,2);
-WithotAwayM2 = nanmean(Pooled_Rate.WithoutAway,2);
-
-M2Rate      = [RippleM2;RippleAwayM2;WithotRFM2;WithotAwayM2];
-trialIdM2   = [Pooled_Rate.TrialIDs;Pooled_Rate.TrialIDs];
-sessionM2   = [Pooled_Rate.session; Pooled_Rate.session];
-
- rippleLabels2 = [ones(length(RippleM2),1);ones(length(RippleAwayM2),1);...
-     zeros(length(WithotRFM2),1);zeros(length(WithotAwayM2),1)];
-
-% pool data of both monkeys
-mnFR = [M1Rate;M2Rate];
-idR = mnFR==0;
-mnFR(idR) = [];
-
-TrialsLabels = [trialIdM1;trialIdM2];
-TrialsLabels(idR,:) = [];
-
-ripplelabels = [rippleLabels1;rippleLabels2];
-ripplelabels(idR) = [];
-sessions  = [sessionM1;sessionM2+26];
-sessions(idR) = [];
-
-tb = table(mnFR,ripplelabels,TrialsLabels(:,1),TrialsLabels(:,2),TrialsLabels(:,3),sessions,...
-    'VariableNames', {'rate','ripple','attention','Focus','size','session'});
-
-tb.ripple = categorical(tb.ripple);
-tb.attention = categorical(tb.attention);
-tb.Focus = categorical(tb.Focus);
-tb.size = categorical(tb.size);
-tb.session = categorical(tb.session);
-
-
- fit_lm = fitlme(tb,'rate ~ 1 + ripple*attention*Focus*size + (1|ripple)','DummyVarCoding','effects');
-
-
-anova(fit_lm)
-
-disp(grpstats(tb,{'ripple','attention'},{'mean','sem'},'DataVars','rate'));
 
 
 
